c3cc4b4bc5163f85c5658d5a75486e5139d82a36
mahmoud <mahmoudBeshay>
2025-12-22 18:11:40 +0200

    addded app env

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -8,3 +8,12 @@ stages {
 		static_code_analysis
 }
 }
+application_environments {
+    dev {
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod {
+        long_name = "Production" 
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
+}
