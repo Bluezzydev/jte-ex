6f5fadf22ada0fe726afe4719c7d53aa44b98a11
Bluezzydev <bluecase007@gmail.com>
2025-12-28 20:19:23 +0200

    Refactor CI stage and add Ansible library

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,13 +1,16 @@
 libraries {
-	maven
+    maven
     sonarqube
+    ansible
 }
+
 stages {
-	cI{
-		build
-		static_code_analysis
+    continuous_integration {
+        build
+        static_code_analysis
+    }
 }
-}
+
 application_environments {
     dev {
         ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
@@ -17,6 +20,7 @@ application_environments {
         ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
     }
 }
+
 keywords {
     requiresApproval = true 
 }
